e757ef2e9b6950f182ff07b2770384e1fa42bc40
Vlad Rogozin <vroghozin@gmail.com>
2023-01-19 15:33:35 +0200

    HW_9

--- lms/settings.py
+++ lms/settings.py
@@ -9,23 +9,23 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+from os import getenv
 from pathlib import Path
+
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7e6t+69g#uw+40_5ahs$8q3qfuxfk5*(rbkr+6=7xv&1sbg(xv'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+SECRET_KEY = getenv('SECRET_KEY')
+DEBUG = getenv('DEBUG', 'False').strip().lower() in ['true', 'on', '1']
+ALLOWED_HOSTS = getenv('ALLOWED_HOSTS', '').split()
 
 
 # Application definition
@@ -126,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if DEBUG:
+    SHELL_PLUS = 'ipython'
+    SHELL_PLUS_PRINT_SQL = True
--- students/views.py
+++ students/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Student
+# HttpRequest
+# HttpResponse
+
+def index(request):
+    students = Student.objects.all().order_by('birthday')
+    # string = '<br>'.join(str(student) for student in students)
+    string = '<table><thead><tr><th>First name</th><th>Last name</th><th>Email</th><th>Birthday</th></tr><thead><tbody>'
+    for st in students:
+        string += f'<fr><td>{st.first_name}</td><td>{st.last_name}</td><td>{st.email}</td><td>{st.birthday}</td></tr>'
+    string += '</tbody></table>'
+    response = HttpResponse(string)
+    return response
+
