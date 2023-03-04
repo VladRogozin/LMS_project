15152d1c52ecdd40d7dc2237d66d1affd11ba788
Vlad Rogozin <vroghozin@gmail.com>
2023-03-04 21:15:21 +0200

    Home work 18

--- Teachers/admin.py
+++ Teachers/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from Teachers.models import Teacher
+
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'salary', 'birthday')
+
